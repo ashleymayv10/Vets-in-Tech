af9be5db3f527f17d90151e170c5007e807d3913
ashleymayv10 <95194923+ashleymayv10@users.noreply.github.com>
2022-03-29 19:19:01 -0400

    update

--- program.js
+++ program.js
@@ -17,31 +17,18 @@ for (var i=0; i < billType.length; i++){
     sel.appendChild(opt);
 }
 
-//Define Variables
-var AmountDue = document.new_item.amount_due.value;
-var Payee = document.new_item.Payee.value;
-var billTypeList = document.new_item.billType.value;
-var dueDate = document.new_item.due_date.value;
-function billData = {
-    document.getElementById("billType").innerHTML = "Bill Type: " + billType;
-    document.getElementById("Payee").innerHTML = "Pay To: " + Payee;
-    document.getElementById("AmountDue").innerHTML = "$" + AmountDue;
-    document.getElementById("dueDate").innerHTML = dueDate;
+
+function billData(event) {
+    event.preventDefault();
+
+    const data = new FormData(event.target);
+
+    const formJSON = Object.fromEntries(data.entries());
+
+    const billData = document.querySelector('.billData pre');
+        billData.innerText = JSON.stringify(formJSON, null, 2)
+
 }
 
-
-// Functions
-
-function addBill(){
-
-
-    
-    // onclick - create new div with field values
-    const createSquare = document.createElement("billSquareUnpaid");
-    
-        // get user data from fields
-        function billData(); 
-       
-    
-    createSquare.appendChild(billType);
-}
+const form = document.querySelector("form");
+form.addEventListener("submit", billData);
